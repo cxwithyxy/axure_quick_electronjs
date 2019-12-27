41a8e9f8a9b2002f5c8650c065efcb8cd9f1c94c
chenxuan <523764720@qq.com>
2019-12-27 15:54:08 +0800

    整理文件缩小子进程的main.ts文件

--- src/renderer_process/Axure_controller.ts
+++ src/renderer_process/Axure_controller.ts
@@ -1,5 +1,28 @@
+import { remote } from "electron";
+import _ from "lodash";
+
 export class Axure_controller
 {
+    function_call_after_main_called?: Function
+    
+    constructor()
+    {
+        this.axure_preload()
+    }
+    
+    axure_preload()
+    {
+        let preload_js_path = _.find(process.argv, (o) =>
+        {
+            return _.startsWith(o,"axure_preload:")
+        })
+        preload_js_path = _.split(<string>preload_js_path, "axure_preload:", 2)[1]
+        if(preload_js_path)
+        {
+            this.function_call_after_main_called = require(preload_js_path).default
+        }
+    }
+
     async get_into_iframe()
     {
         return new Promise(succ =>
