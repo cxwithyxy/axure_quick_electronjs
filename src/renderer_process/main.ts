57fc227db141017512315f12b15c8b7fc98ed233
chenxuan <523764720@qq.com>
2019-12-27 11:24:14 +0800

    修复假如没有设置注入子进程的js路径时报错的问题

--- src/renderer_process/main.ts
+++ src/renderer_process/main.ts
@@ -18,7 +18,10 @@ class Main_controller
             return _.startsWith(o,"axure_preload:")
         })
         preload_js_path = _.split(<string>preload_js_path, "axure_preload:", 2)[1]
-        this.function_call_after_main_called = require(preload_js_path).default
+        if(preload_js_path)
+        {
+            this.function_call_after_main_called = require(preload_js_path).default
+        }
     }
 }
 
